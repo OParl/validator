1da9d3156ab71acf3f721549738be94e051d51f5
Telofy <telofy+git@yoursiblings.org>
2014-05-28 09:38:32 +0200

    Added incomplete meeting schema

--- oparlvalidator/schema.py
+++ oparlvalidator/schema.py
@@ -2,74 +2,158 @@
 from __future__ import (unicode_literals, absolute_import,
                         division, print_function)
 
-# As a rare exception. I think it’ll be helpful to use double quotes for
-# the schema, so it doubles as JSON. Mind the commas.
-
 OPARL = {
-    "id": "http://some.site.somewhere/entry-schema#",  # TODO
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "description": "OParl schema",
-    "type": "object",
-    "properties": {
-        "anyOf": [
-            {"$ref": "#/definitions/system"},
-            {"$ref": "#/definitions/body"},
-            {"$ref": "#/definitions/organization"},
-            {"$ref": "#/definitions/person"},
-            {"$ref": "#/definitions/meeting"},
-            {"$ref": "#/definitions/agendaItem"},
-            {"$ref": "#/definitions/paper"},
-            {"$ref": "#/definitions/document"},
-            {"$ref": "#/definitions/consultation"},
-            {"$ref": "#/definitions/location"},
-            {"$ref": "#/definitions/membership"}
-        ]
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'description': 'OParl schema',
+    'type': 'object',
+    'properties': {
+        'system': {
+            'type': {'$ref': '#/definitions/system'},
+        },
+        'body': {
+            'type': {'$ref': '#/definitions/body'},
+        },
+        'organization': {
+            'type': {'$ref': '#/definitions/organization'},
+        },
+        'person': {
+            'type': {'$ref': '#/definitions/person'},
+        },
+        'meeting': {
+            'type': {'$ref': '#/definitions/meeting'},
+        },
+        'agendaItem': {
+            'type': {'$ref': '#/definitions/agendaItem'},
+        },
+        'paper': {
+            'type': {'$ref': '#/definitions/paper'},
+        },
+        'document': {
+            'type': {'$ref': '#/definitions/document'},
+        },
+        'consultation': {
+            'type': {'$ref': '#/definitions/consultation'},
+        },
+        'location': {
+            'type': {'$ref': '#/definitions/location'},
+        },
+        'membership': {
+            'type': {'$ref': '#/definitions/membership'},
+        },
     },
-    "definitions": {
-        "system": {},  # TODO: Move newObjects, updatedObjects,
+    'definitions': {
+        'system': {},  # TODO: Move newObjects, updatedObjects,
                        # and removedObjects
-        "body": {},
-        "organization": {},
-        "person": {},
-        "meeting": {},
-        "agendaItem": {},
-        "paper": {},
-        "document": {},
-        "consultation": {},
-        "location": {},
-        "membership": {
-            "type": "object",
-            "properties": {
-                "person": {
-                    "type": "string",
-                    "format": "uri"
+        'body': {},
+        'organization': {},
+        'person': {},
+        'meeting': {
+            'type': 'object',
+            'properties': {
+                '@context': {},
+                '@type': {
+                    'type': 'string',
                 },
-                "organization": {
-                    "type": "string",
-                    "format": "uri"
+                '@id': {
+                    'type': 'string',
                 },
-                "role": {
-                    "type": "string"
+                'name': {
+                    'type': 'string',
                 },
-                "post": {
-                    "type": "string"
+                'start': {
+                    'type': 'date-time',
                 },
-                "onBehalfOf": {
-                    "type": "string",
-                    "format": "uri"
+                'end': {
+                    'type': 'date-time',
                 },
-                "startDate": {
-                    "type": "string",
-                    "format": "date-time"
+                'location': {
+                    '$ref': '#/definitions/location',
                 },
-                "endDate": {
-                    "type": "string",
-                    "format": "date-time"
+                'organization': {
+                    '$ref': '#/definitions/location',
+                },
+                'participant': {
+                    'type': 'array',
+                    'items': {
+                        '$ref': '#/definitions/person',
+                    },
+                },
+                'invitation': {
+                    '$ref': '#/definitions/document',
+                },
+                'resultsProtocol': {
+                    '$ref': '#/definitions/document',
+                },
+                'verbatimProtocol': {
+                    '$ref': '#/definitions/document',
+                },
+                'auxiliaryDocument': {
+                    'type': 'array',
+                    'items': {
+                        '$ref': '#/definitions/document',
+                    },
+                },
+                'agendaItem': {
+                    'type': 'array',
+                    'items': {
+                        '$ref': '#/definitions/agendaItem',
+                    },
+                },
+                'created': {
+                    'type': 'date-time',
+                },
+                'modified': {
+                    'type': 'date-time',
+                },
+            },
+            'required': [
+                '@context',
+                '@id',
+                '@type',
+                'name',
+                'organization',
+                'participant',
+                'start',
+            ],
+        },
+        'agendaItem': {},
+        'paper': {},
+        'document': {},
+        'consultation': {},
+        'location': {},
+        'membership': {
+            'type': 'object',
+            'properties': {
+                'person': {
+                    'type': 'string',
+                    'format': 'uri'
+                },
+                'organization': {
+                    'type': 'string',
+                    'format': 'uri'
+                },
+                'role': {
+                    'type': 'string'
+                },
+                'post': {
+                    'type': 'string'
+                },
+                'onBehalfOf': {
+                    'type': 'string',
+                    'format': 'uri'
+                },
+                'startDate': {
+                    'type': 'string',
+                    'format': 'date-time'
+                },
+                'endDate': {
+                    'type': 'string',
+                    'format': 'date-time'
                 }
             },
-            "required": [
-                "person",
-                "organization"
+            'required': [
+                'person',
+                'organization'
             ]
         }
     }
