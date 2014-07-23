52622167558a5818a256f728a5b3aeff08d9fd24
Alexander Sulfrian <alexander@sulfrian.net>
2014-07-23 20:57:36 +0200

    server_tests: fix python style

--- oparlvalidator/server_tests.py
+++ oparlvalidator/server_tests.py
@@ -1,34 +1,30 @@
 # -*- encoding: utf-8 -*-
-from __future__ import (unicode_literals, absolute_import,
-                        division, print_function)
 """
 Collection of non-typespecific tests.
 """
 
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
 import requests
 
 
-class ServerTests():
-    # TODO: doc me
+def check_accecpt_encoding(url):
+    """
+    Pass an url and check wether mandatory compressions are supported.
 
-    def check_accecpt_encoding(self, url):
-        """
-        Pass an url and check wether mandatory compressions are supported.
+    Args:
+        url: URL to some json
 
-        Args:
-            url: URL to some json
+    Returns:
+        True on success, false otherwise.
+    """
+    compressions = ["gzip", "compress", "deflate"]
+    header = {"Accept-Encoding": ", ".join(compressions)}
+    response = requests.get(url, header=header)
 
-        Returns:
-            True on success, false otherwise.
-        """
-        compressions = ["gzip", "compress", "deflate"]
-        response = requests.get(
-            url,
-            {"Accept-Encoding": ", ".join(compressions)}
-        )
-        # response shall contain header w/ "Content-Encoding" wich must be
-        # any of the above
-        for compression in compressions:
-            if compression in response.header["Content-Encoding"]:
-                return True
-        return False
+    # response shall contain header w/ "Content-Encoding" wich must be
+    # any of the above
+    for compression in compressions:
+        if compression in response.header["Content-Encoding"]:
+            return True
+    return False
