e2ed2f8077018270a6ce29b7e4a9396ac7c7dd1a
luuu <luuu@users.noreply.github.com>
2014-08-01 21:02:12 +0200

    use new testserver in crawler tests

--- oparlvalidator/tests/test_crawler.py
+++ oparlvalidator/tests/test_crawler.py
@@ -3,9 +3,11 @@ from __future__ import (unicode_literals, absolute_import,
                         division, print_function)
 import threading
 import unittest
+import json
 import static
 from wsgiref.simple_server import make_server
 from os.path import join, dirname
+from os import walk
 from ..crawler import Crawler
 from .server import Server
 from ..server_tests import (
@@ -19,14 +21,22 @@ class TestCrawler(unittest.TestCase):
     # pylint: disable=protected-access
 
     def setUp(self):
-        wsgi_app = static.Cling(DATA_DIR)
-        self.httpd = make_server('', 2342, wsgi_app)
-        httpd_thread = threading.Thread(target=self.httpd.serve_forever)
-        httpd_thread.setDaemon(True)
-        httpd_thread.start()
+        self.server = Server()
+        self._fix_url(
+            Crawler, 'http://{}:{}/'
+            .format(self.server.host, self.server.port))
+
+        self.testdata = {}
+        import os
+        for root, _, files in walk(DATA_DIR):
+            for filename in files:
+                if filename.endswith(".valid.json"):
+                    filepath = os.path.join(root, filename)
+                    with open(filepath) as json_file:
+                        self.testdata[filename] = json.load(json_file)
 
     def tearDown(self):
-        self.httpd.shutdown()
+        self.server.shutdown()
 
     def _fix_url(self, crawler, host):
         orig_retrieve = crawler._retrieve
@@ -35,12 +45,6 @@ class TestCrawler(unittest.TestCase):
             url = args[0].replace('https://oparl.example.org/', host)
             return orig_retrieve(self, url, **kwargs)
         crawler._retrieve = _fixUrl
-
-    def test_something(self):
-        self._fix_url(Crawler, 'http://localhost:2342/')
-        # TODO: Improve test data, then raise limit
-        Crawler('https://oparl.example.org/person.valid.json',
-                max_documents=0).run()
 
     def test_accept_encoding(self):
         """
@@ -77,7 +81,7 @@ class TestCrawler(unittest.TestCase):
         server.serve(test_cases)
 
         # run assertions
-        prefix = 'http://localhost:%s' % server.port
+        prefix = 'http://localhost:%s' % self.server.port
         self.assertEquals(True,
                           check_accept_encoding(prefix + '/example/valid'))
         self.assertEquals(False,
@@ -90,7 +94,6 @@ class TestCrawler(unittest.TestCase):
                           check_accept_encoding(
                               prefix + '/example/invalid_type_not_supported')
                           )
-        server.shutdown()
 
     def test_invalid_url_params(self):
         """
@@ -141,21 +144,19 @@ class TestCrawler(unittest.TestCase):
         """
         Test for upper http status codes (Section 4.12).
         """
-        # setup server
-        server = Server()
-        server.serve({
+        self.server.serve({
             '/ressource/exists': {
                 'GET': {
                     'headers': [('status', '200 OK')]
                 }
             }
         })
-        prefix = 'http://localhost:%s' % server.port
+        prefix = 'http://localhost:%s' % self.server.port
         self.assertEquals(False,
                           check_http_status_codes(prefix + '/ressource/exists')
                           )
 
-        server.serve({
+        self.server.serve({
             '/ressource': {
                 'GET': {
                     'headers': [('status', '200 OK')]
@@ -170,4 +171,3 @@ class TestCrawler(unittest.TestCase):
         self.assertEquals(True,
                           check_http_status_codes(prefix + '/ressource')
                           )
-        server.shutdown()
