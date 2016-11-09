1eecd18f2fff7f106383462d128f6156f3925ce8
Stefan Graupner <stefan.graupner@gmail.com>
2016-11-09 11:24:29 +0100

    Removing obsolete validator implementation

--- oparlvalidator/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- encoding: utf-8 -*-
-from __future__ import (unicode_literals, absolute_import,
-                        division, print_function)
-from .validator import OParlJson, OParlResponse
