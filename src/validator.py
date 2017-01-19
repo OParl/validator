7901385d556960dcf90800372a76b340e0871c08
Stefan Graupner <stefan.graupner@gmail.com>
2017-01-19 12:59:29 +0100

    Wrap everything into a Validator class

--- src/validator.py
+++ src/validator.py
@@ -7,18 +7,26 @@ gi.require_version('OParl', '0.2')
 
 from gi.repository import OParl
 
-def resolve(_, url):
-    try:
-        r = requests.get(url)
-        r.raise_for_status()
+class Validator:
+    url = ""
+    system = None
 
-        return r.text
-    except Exception as e:
-        return None
+    def __init__(self, url):
+        self.url = url
+
+        client = OParl.Client()
+        client.connect("resolve_url", Validator.resolve_url)
+
+        self.system = client.open(url)
+
+    def resolve_url(_, url):
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+
+            return r.text
+        except Exception as e:
+            return None
 
 if __name__ == "__main__":
-    client = OParl.Client()
-    client.connect("resolve_url", resolve)
-    system = client.open(sys.argv[1])
-
-    print(system.get_name())
+    validator = Validator(sys.argv[1])
