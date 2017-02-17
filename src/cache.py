541c0d4679406302ad9106249c725dae1e5a3e08
Stefan Graupner <stefan.graupner@gmail.com>
2017-02-17 11:51:46 +0100

    stub cache, extract specific redis cache

--- src/cache.py
+++ src/cache.py
@@ -26,10 +26,23 @@ import redis
 class Cache:
     basekey = ""
     ttl = 300
+
+    def __init__(self, url, ttl=300):
+        self.basekey = hashlib.sha1(url.encode('ascii')).hexdigest()
+        self.ttl = ttl
+
+    def has(self, key):
+        return False
+
+    def get(self, key):
+        return ""
+
+    def put(self, key, ttl=300):
+        pass
+
+class RedisCache(Cache):
     rclient = None
 
     def __init__(self, url, ttl=300, redis_server='localhost', redis_port=6379):
-        self.basekey = hashlib.sha1(url.encode('ascii')).hexdigest()
-        self.ttl = ttl
+        self = Cache(url, ttl)
         self.rclient = redis.ConnectionPool(host=redis_server, port=redis_port, db=0)
-
