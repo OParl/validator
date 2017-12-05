b8d4aa11a19bd99ae8300fdf95b6628c320ca8b7
Stefan Graupner <stefan.graupner@gmail.com>
2017-12-05 14:19:08 +0100

    Use signals for incoming pages to speed up data fetch
    Liboparl offers signals for incoming object pages
    which are now being used to radically speed up the data
    fetching process by pushing new objects to the queue
    as soon as they arrive.
    
    This adds a little more strain on the memory however,
    as body walkers keep their own little seen lists
    to fend of duplicate objects in several pages.

--- core/body_walker.py
+++ core/body_walker.py
@@ -26,6 +26,7 @@ from threading import Thread
 
 from .utils import sha1_hexdigest
 from .output import Output
+from .seen_list import SeenList
 
 class BodyWalker(Thread):
     def __init__(self, client, body, queue):
@@ -34,24 +35,59 @@ class BodyWalker(Thread):
         self.body = body
         self.queue = queue
         self.done = False
+        self.seen_list = SeenList()
+
+    def connect_signals(self):
+        incoming_signals = [
+            'incoming_organizations',
+            'incoming_meetings',
+            'incoming_papers',
+            'incoming_persons'
+        ]
+
+        for incoming_signal in incoming_signals:
+            self.body.connect(incoming_signal, self.handle_incoming)
 
     def run(self):
         progress_id = sha1_hexdigest(self.body.get_id())
-        Output.add_progress_bar(progress_id, 'Fetching from \'{}\''.format(self.body.get_name()))
+        #Output.add_progress_bar(progress_id, 'Fetching from \'{}\''.format(self.body.get_name()))
+
+        self.connect_signals()
 
         try:
-            neighbors = self.body.get_neighbors()
+            self.body.get_neighbors()
         except GLib.Error:
             Output.message('Body {} failed to provide entities', self.body.get_id())
             return
 
-        total_neighbors = len(neighbors)
+        # total_neighbors = len(neighbors)
 
-        for index, neighbor in enumerate(neighbors):
-            self.queue.acquire()
-            self.queue.put(neighbor)
-            self.queue.release()
+        # for index, neighbor in enumerate(neighbors):
 
-            Output.update_progress_bar(progress_id, remaining=total_neighbors - index - 1)
 
-        Output.message("Fetched {} objects from {}", total_neighbors, self.body.get_id())
+        #Output.update_progress_bar(progress_id, remaining=total_neighbors - index - 1)
+
+        self.queue.acquire()
+        self.queue.put(self.body)
+        self.queue.release()
+
+        #Output.message("Fetched {} objects from {}", total_neighbors, self.body.get_id())
+
+    def handle_incoming(self, body, object_list):
+        num_new_objects = len(object_list)
+        Output.message('Received {} new objects from {}', num_new_objects, body.get_name())
+
+        for index, entity in enumerate(object_list):
+            if not self.is_seen_entity(entity):
+                self.queue.acquire()
+                self.queue.put(entity)
+                self.queue.release()
+
+    # NOTE: this is very similar to ValidationWorker.is_seen_object...
+    def is_seen_entity(self, entity):
+        if entity.get_id() in self.seen_list:
+            return True
+
+        self.seen_list.push(entity.get_id())
+
+        return False
