f2d1e83923efb1d5b1f8d884f0cb4121678f1ec7
Stefan Graupner <efrane@meanderingsoul.com>
2017-12-21 11:28:12 +0100

    Introduce setup.py for packaging purposes

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    author='Stefan "eFrane" Graupner',
+    author_email='stefan.graupner@oparl.org',
+    description = 'Validator for OParl data',
+    install_requires=[
+        'beautifultable'
+        'redis'
+        'requests'
+        'tqdm'
+    ],
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read(),
+    name = 'oparl-validator',
+    packages=['core', 'extra'],
+    url='https://github.com/OParl/validator',
+    version= 'master'
+)
